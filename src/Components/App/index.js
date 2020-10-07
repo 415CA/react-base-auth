05f52659a237c121f4bb909418f815179d5e37a2
415CA <chrisallen415@gmail.com>
2020-10-07 12:00:04 -0400

    inital commit

---
+++ src/Components/App/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../../Pages/Landing';
+import SignUpPage from '../Authentication/SignUp';
+import SignInPage from '../Authentication/SignIn';
+import PasswordForgetPage from '../Authentication/PasswordForget';
+import HomePage from '../../Pages/Home';
+import AccountPage from '../Authentication/Account';
+import AdminPage from '../Authentication/Admin';
+
+import * as ROUTES from '../../Constants/routes';
+import { withAuthentication } from '../Authentication/Session';
+
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
+);
+
+export default withAuthentication(App);
---
+++ src/Components/Navigation/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AuthUserContext } from '../Authentication/Session';
+
+import SignOutButton from '../Authentication/SignOut';
+import * as ROUTES from '../../Constants/routes';
+import * as ROLES from '../../Constants/roles';
+
+const Navigation = () => (
+  <AuthUserContext.Consumer>
+    {(authUser) =>
+      authUser ? <NavigationAuth authUser={authUser} /> : <NavigationNonAuth />
+    }
+  </AuthUserContext.Consumer>
+);
+
+const NavigationAuth = ({ authUser }) => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    {!!authUser.roles[ROLES.ADMIN] && (
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    )}
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
+);
+
+export default Navigation;
